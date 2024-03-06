784838524b406ae07b0f6370d5d8a9de02780ad1
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-03-06 11:52:37 +0200

    Simple cmd console

--- console.py
+++ console.py
@@ -6,17 +6,17 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """Quit command to exit the program"""
+        """Quit command to exit the program """
         return True
     # Aliasing
     do_exit = do_quit
 
     def do_EOF(self, arg):
-        """Exit the program with Ctrl+D (EOF)"""
+        """Exit the program with Ctrl+D (EOF) """
         return True
 
     def emptyline(self):
-        """Do nothing when an empty line is entered"""
+        """Do nothing when an empty line is entered """
         pass
 
 if __name__ == '__main__':
